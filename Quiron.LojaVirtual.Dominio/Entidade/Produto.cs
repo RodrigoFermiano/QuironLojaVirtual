e48de9ae190a74611906217b24491115d9477050
Rodrigo Fermiano <rc.fermiano@hotmail.com>
2015-10-31 17:23:55 -0200

    Commit 3

--- Quiron.LojaVirtual.Dominio/Entidade/Produto.cs
+++ Quiron.LojaVirtual.Dominio/Entidade/Produto.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace Quiron.LojaVirtual.Dominio.Entidade
 {
